841843a51c84b9737b4ab5eb350d0d36f9aa228c
Jorge <jfgf11@gmail.com>
2020-05-09 23:33:54 -0500

    mas arreglos a imu

--- scripts/graficador.py
+++ scripts/graficador.py
@@ -3,12 +3,9 @@
 import rospy
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from geometry_msgs.msg import Twist
-from std_msgs.msg import Float32MultiArray
 from gazebo_msgs.msg import ModelStates
 import mpl_toolkits.mplot3d.axes3d as p3
-
-
+from geometry_msgs.msg import Pose
 
 #En este metodo se inicializa el nodo y se suscribe a los topicos de la posicion actual del robot y los puntos detectados.
 class graficardor():
@@ -18,6 +15,7 @@ class graficardor():
         axs = p3.Axes3D(self.fig)
         rospy.init_node('graficador', anonymous=True)
         rospy.Subscriber('/gazebo/model_states', ModelStates, self.model_states_callback)
+        rospy.Subscriber('/imu1/pos', Pose, self.imu_1)
         self.ani = animation.FuncAnimation(self.fig, self.animate)
         plt.show()
     
@@ -27,15 +25,23 @@ class graficardor():
         y_gazebo = msg.pose[1].position.y
         z_gazebo = msg.pose[1].position.z
         pass
+    def imu_1(self, msg):
+        global x_imu_1, y_imu_1, z_imu_1
+        x_imu_1 = msg.position.x
+        y_imu_1 = msg.position.y
+        z_imu_1 = msg.position.z
+        pass
+            
 
     def animate(i,j):
-        global axs, x_gazebo, y_gazebo, z_gazebo
+        global axs, x_gazebo, y_gazebo, z_gazebo, x_imu_1, y_imu_1, z_imu_1
         axs.clear()
         axs.set_xlim3d([-50, 50])
         axs.set_ylim3d([-50, 50])
         axs.set_zlim3d([-50, 0])
         axs.scatter(x_gazebo, y_gazebo,z_gazebo, c = 'b')
-        plt.title('Rover real time position')
+        axs.scatter(x_imu_1, y_imu_1, z_imu_1, c = 'r')
+        plt.title('ROV real time position')
         plt.grid()
 
 if __name__ == '__main__':
--- scripts/imu_i.py
+++ scripts/imu_i.py
@@ -7,7 +7,6 @@ import math
 import sys, unittest
 import os, os.path, time
 import rospy
-from std_msgs.msg import String
 from geometry_msgs.msg import Pose, Quaternion, Point, PoseStamped, PoseWithCovariance, TwistWithCovariance, Twist, Vector3, Wrench
 from sensor_msgs.msg import Imu
 from gazebo_msgs.msg import ModelStates
@@ -15,17 +14,22 @@ from gazebo_msgs.msg import ModelStates
 class imu1_node:
     def __init__(self,):
     	print('imu1_node: initializing node')
-    	self.Pose_IMU = POSE()
+    	PoseIMU = Pose()
     	self.done = False
     	self.imu_vx = 0
         self.imu_vy = 0
         self.imu_vz = 0
+        self.imu_x	= 0
+        self.imu_y = 0
+        self.imu_z = 0
         self.first = True
+        self.done = False
         rospy.Subscriber('/gazebo/model_states', ModelStates, self.model_states_callback)
        	rospy.Subscriber('/imu1/imu', Imu, self.imu_callback)
        	imu_pos = rospy.Publisher('/imu1/pos', Pose, queue_size=10)
+       	rate = rospy.Rate(50)
        	while not rospy.is_shutdown():
-       		if self.done = False: 
+       		if self.done: 
        			PoseIMU.position.x = self.imu_x	
        			PoseIMU.position.y = self.imu_y
        			PoseIMU.position.z = self.imu_z
@@ -33,18 +37,19 @@ class imu1_node:
        			PoseIMU.orientation.y = 0
        			PoseIMU.orientation.z = 0
        			PoseIMU.orientation.w = 0
-       			imu_pos.push(PoseIMU)
-       			
+       			imu_pos.publish(PoseIMU)
+       			#print(PoseIMU.position.x)
        		rate.sleep()
 
     def imu_callback(self, msg):
-    	if self.first = False:
-	    	self.imu_vx = self.imu_vx + msg.linear_acceleration.x*(1/50)
-	        self.imu_vy = self.imu_vy + msg.linear_acceleration.y*(1/50)
-	        self.imu_vz = self.imu_vz + msg.linear_acceleration.z*(1/50)
-	    	self.imu_x = self.imu_x + self.imu_vx*(1/50)
-	        self.imu_y = self.imu_y + self.imu_vy*(1/50)
-	        self.imu_z = self.imu_z + self.imu_vz*(1/50)
+    	if self.first == False:
+	    	self.imu_vx = self.imu_vx + msg.linear_acceleration.x*(1/float(50))
+	        self.imu_vy = self.imu_vy + msg.linear_acceleration.y*(1/float(50))
+	        self.imu_vz = self.imu_vz + (msg.linear_acceleration.z-9.8)*(1/float(50))
+	    	self.imu_x = self.imu_x + self.imu_vx/float(50) + msg.linear_acceleration.x*(1/float(50))**2
+	        self.imu_y = self.imu_y + self.imu_vy/float(50) + msg.linear_acceleration.y*(1/float(50))**2
+	        self.imu_z = self.imu_z + self.imu_vz/float(50) + (msg.linear_acceleration.z-9.8)*(1/float(50))**2
+	        #print(msg.linear_acceleration.z)
 	        self.done = True
         pass
 
